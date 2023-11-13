99c6e52b7e462b0124b8792944e4807495661b6f
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-13 23:47:48 +0600

    use useMenu custom hook to load menu data

--- src/components/Home/PopularMenu/PopularMenu.jsx
+++ src/components/Home/PopularMenu/PopularMenu.jsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  const [menu] = useMenu();
+  const popular = menu.filter((item) => item.category === "popular");
 
   return (
     <section className="container mx-auto">
@@ -23,7 +15,7 @@ const PopularMenu = () => {
         ></SectionTitle>
       </div>
       <div className="grid grid-cols-2 gap-4 mb-20">
-        {menu.map((item) => (
+        {popular.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
