f28ca028c57a6a5fe2e04988699f1952a9bd34f9
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-14 22:24:08 +0600

    loaded menu and reviews data from server instead of local folder

--- src/components/Home/Testimonials/Testimonials.jsx
+++ src/components/Home/Testimonials/Testimonials.jsx
@@ -12,7 +12,7 @@ const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
-    fetch("reviews.json")
+    fetch("http://localhost:5000/reviews")
       .then((res) => res.json())
       .then((data) => {
         setReviews(data);
@@ -29,8 +29,8 @@ const Testimonials = () => {
       </div>
       <div>
         <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-          {reviews.map((review) => (
-            <SwiperSlide key={review._id}>
+          {reviews?.map((review) => (
+            <SwiperSlide key={review?._id}>
               <div className="px-20">
                 <div className="flex flex-col items-center justify-center">
                   <Rating
