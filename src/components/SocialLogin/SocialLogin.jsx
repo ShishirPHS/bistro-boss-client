47cd351c1f72741fd676ca0ec2882f6312ab3e27
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-18 00:55:13 +0600

    navigate user after google login & send user's info to the server side

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,13 +1,30 @@
 import { FaGoogle } from "react-icons/fa6";
 import useAuth from "../../hooks/useAuth";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const SocialLogin = () => {
   const { loginWithGoogle } = useAuth();
+  const axiosPublic = useAxiosPublic();
+  const navigate = useNavigate();
 
   const handleGoogleLogin = () => {
     loginWithGoogle()
       .then((res) => {
         console.log(res.user);
+        const userInfo = {
+          name: res.user.displayName,
+          email: res.user.email,
+        };
+        axiosPublic.post("/users", userInfo).then((res) => {
+          console.log(res.data);
+          Swal.fire({
+            text: "Login Successful",
+            icon: "success",
+          });
+          navigate("/");
+        });
       })
       .catch((err) => {
         console.log(err.message);
