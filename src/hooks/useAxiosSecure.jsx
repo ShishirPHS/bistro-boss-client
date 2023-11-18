aca719f3882559a2e99862befe2b6313437c5aa4
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-19 01:39:20 +0600

    logout and navigate user if error status is 401 or 403

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,48 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000/",
 });
 
 const useAxiosSecure = () => {
+  const { logOutUser } = useAuth();
+  const navigate = useNavigate();
+
+  // request interceptor to add authorization header for every secure call to the api
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      const token = localStorage.getItem("access-token");
+      // console.log("request stopped by interceptors", token);
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+
+  // interceptor 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      console.log("status error in the interceptor: ", status);
+
+      // for 401 and 403 logout the use and move the user to the login page
+      if (status === 401 || status === 403) {
+        await logOutUser();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return axiosSecure;
 };
 
