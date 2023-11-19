899d77f0303e1d60fb3f7874740e5e821871b243
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-20 04:57:50 +0600

    added addItem functionality

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,10 +1,49 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/Home/SectionTitle/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const { register, handleSubmit, reset } = useForm();
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    // image upload to imgbb and then get an url
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    if (res.data.success) {
+      // now send the menu item data to the server with the image url
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      };
+      const menuRes = await axiosSecure.post("/menu", menuItem);
+      console.log(menuRes.data);
+      if (menuRes.data.insertedId) {
+        // show success pop-up
+        reset();
+        Swal.fire({
+          text: `${data.name} added to the menu.`,
+          icon: "success",
+        });
+      }
+    }
+    console.log("with image url", res.data);
+  };
 
   return (
     <div>
@@ -72,7 +111,7 @@ const AddItems = () => {
               </span>
             </label>
             <textarea
-              {...register("recipeDetails", { required: true })}
+              {...register("recipe", { required: true })}
               className="textarea textarea-bordered"
               placeholder="Recipe Details"
             ></textarea>
