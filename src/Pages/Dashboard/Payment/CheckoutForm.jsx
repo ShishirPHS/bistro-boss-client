8003cc7aafbf3ae5b847bca26a33b369ebf494a8
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-22 19:50:09 +0600

    used confirmCardPayment to process payment

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -2,13 +2,16 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
+import useAuth from "../../../hooks/useAuth";
 
 const CheckoutForm = () => {
   const [error, setError] = useState("");
   const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
   const stripe = useStripe();
   const elements = useElements();
   const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
   const [cart] = useCart();
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
 
@@ -46,6 +49,26 @@ const CheckoutForm = () => {
       console.log("Payment method", paymentMethod);
       setError("");
     }
+    // confirm payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+    if (confirmError) {
+      console.log("confirm error: ", confirmError);
+    } else {
+      console.log("payment intent: ", paymentIntent);
+      if (paymentIntent.status === "succeeded") {
+        console.log("transaction id: ", paymentIntent.id);
+        setTransactionId(paymentIntent.id);
+      }
+    }
   };
 
   return (
@@ -66,7 +89,6 @@ const CheckoutForm = () => {
           },
         }}
       />
-      <p className="text-red-600">{error}</p>
       <button
         className="btn btn-primary px-7 mt-3"
         type="submit"
@@ -74,6 +96,11 @@ const CheckoutForm = () => {
       >
         Pay
       </button>
+      <p className="text-red-600">{error}</p>
+
+      {transactionId && (
+        <p className="text-green-600">Your transaction id: {transactionId}</p>
+      )}
     </form>
   );
 };
