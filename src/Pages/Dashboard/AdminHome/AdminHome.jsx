da7fccede5129941e98f8a084b289b76716e07a5
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-24 03:41:40 +0600

    shown statistics to admin home

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,7 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaBook, FaDollarSign, FaTruckMoving, FaUsers } from "react-icons/fa6";
 
 const AdminHome = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure("/admin-stats");
+      return res.data;
+    },
+  });
 
   return (
     <div>
@@ -9,6 +21,43 @@ const AdminHome = () => {
         <span>Hi, Welcome </span>
         {user?.displayName ? user?.displayName : "Back"}
       </h2>
+      <div className="stats shadow mt-12">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-3xl"></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats?.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl"></FaUsers>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats?.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook className="text-3xl"></FaBook>
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats?.menuItems}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaTruckMoving className="text-3xl"></FaTruckMoving>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats?.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
     </div>
   );
 };
